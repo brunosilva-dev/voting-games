ad2374fac3fb2a47be119b5320430037f1547b8d
Bruno Silva <silva.dev@hotmail.com>
2025-01-21 09:36:31 -0300

    Subindo componente criado para site de votação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,102 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
-export default function Home() {
+const Page: React.FC = () => {
+  const [selectedPhoto, setSelectedPhoto] = useState<number | null>(null);
+
+  const photos = [
+    { id: 1, src: "/assets/cold-war.jpeg", alt: "Call Of Duty Cold War" },
+    { id: 2, src: "/assets/dredge.jpeg", alt: "Dredge" },
+    {
+      id: 3,
+      src: "/assets/kingdom-come-deliverance.jpeg",
+      alt: "Kingdom Come Deliverance",
+    },
+    { id: 4, src: "/assets/little-nightmare.jpg", alt: "Little Nightmare" },
+    { id: 5, src: "/assets/mafia-2.jpg", alt: "Mafia 2" },
+    {
+      id: 6,
+      src: "/assets/murdered-soul-suspect.jpeg",
+      alt: "Murdered Soul Suspect",
+    },
+    { id: 7, src: "/assets/sinking-city.jpeg", alt: "The Sinking City" },
+    { id: 8, src: "/assets/the last of us.jpg", alt: "The Last Of Us" },
+    { id: 9, src: "/assets/the-witcher-3.jpeg", alt: "The Witcher 3" },
+    { id: 10, src: "/assets/twin-mirror.jpg", alt: "The Mirror" },
+  ];
+
+  const [votes, setVotes] = useState<number[]>(
+    new Array(photos.length).fill(0)
+  ); // Contador de votos por foto
+
+  const handleVote = () => {
+    if (selectedPhoto !== null) {
+      // Atualiza o contador de votos
+      const newVotes = [...votes];
+      newVotes[selectedPhoto - 1] += 1; // Adiciona 1 voto à foto selecionada
+      setVotes(newVotes);
+
+      alert(`Você votou no jogo: ${photos[selectedPhoto - 1].alt}`);
+    } else {
+      alert("Por favor, selecione uma foto antes de votar!");
+    }
+  };
+
+  // Calcular a porcentagem de votos para cada foto
+  const totalVotes = votes.reduce((acc, vote) => acc + vote, 0);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <h1>Qual jogo gostaria de ver em live?</h1>
+        <p>Você tem 1 voto disponível</p>
+      </header>
+      <main className={styles.gallery}>
+        {photos.map((photo, index) => {
+          const votePercentage =
+            totalVotes > 0 ? (votes[index] / totalVotes) * 100 : 0; // Calcula a porcentagem de votos
+          return (
+            <div
+              key={photo.id}
+              className={`${styles.photo} ${
+                selectedPhoto === photo.id ? styles.selected : ""
+              }`}
+              onClick={() => setSelectedPhoto(photo.id)}
+            >
+              <Image
+                src={photo.src}
+                alt={photo.alt}
+                width={150}
+                height={150}
+                className="photo"
+              />
+              <div className={styles.photoTextContainer}>
+                <p>{photo.alt}</p>
+              </div>
+              <p className={styles.percentual}>
+                {Math.round(votePercentage)}% - {votes[index]} votos
+              </p>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+              <div className={styles.voteBarContainer}>
+                <div
+                  className={styles.voteBar}
+                  style={{ width: `${votePercentage}%` }}
+                ></div>
+              </div>
+            </div>
+          );
+        })}
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <button onClick={handleVote} className={styles.voteButton}>
+          Vote
+        </button>
       </footer>
     </div>
   );
-}
+};
+
+export default Page;
