a482a9381b2ca2ef084d5a6ec030abfbdfb070c0
Bruno Silva <silva.dev@hotmail.com>
2025-01-21 17:53:44 -0300

    Atualização fonte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,129 @@
+// "use client";
+
+// import React, { useState } from "react";
+// import styles from "./page.module.css";
+// import Image from "next/image";
+
+// const Page: React.FC = () => {
+//   const [selectedPhoto, setSelectedPhoto] = useState<number | null>(null);
+//   const [isVoting, setIsVoting] = useState(false); // Para desativar o botão enquanto vota
+//   const [votes, setVotes] = useState<number[]>(new Array(10).fill(0));
+
+// const photos = [
+//   { id: 1, src: "/assets/cold-war.jpeg", alt: "Call Of Duty Cold War" },
+//   { id: 2, src: "/assets/dredge.jpeg", alt: "Dredge" },
+//   {
+//     id: 3,
+//     src: "/assets/kingdom-come-deliverance.jpeg",
+//     alt: "Kingdom Come Deliverance",
+//   },
+//   { id: 4, src: "/assets/little-nightmare.jpg", alt: "Little Nightmare" },
+//   { id: 5, src: "/assets/mafia-2.jpg", alt: "Mafia 2" },
+//   {
+//     id: 6,
+//     src: "/assets/murdered-soul-suspect.jpeg",
+//     alt: "Murdered Soul Suspect",
+//   },
+//   { id: 7, src: "/assets/sinking-city.jpeg", alt: "The Sinking City" },
+//   { id: 8, src: "/assets/the last of us.jpg", alt: "The Last Of Us" },
+//   { id: 9, src: "/assets/the-witcher-3.jpeg", alt: "The Witcher 3" },
+//   { id: 10, src: "/assets/twin-mirror.jpg", alt: "The Mirror" },
+// ];
+
+//   const handleVote = async () => {
+//     if (selectedPhoto === null) {
+//       alert("Por favor, selecione uma foto antes de votar!");
+//       return;
+//     }
+
+//     setIsVoting(true); // Desativa o botão enquanto processa
+
+//     try {
+//       const response = await fetch("/api/vote", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({
+//           photoId: photos[selectedPhoto - 1].id,
+//           photoAlt: photos[selectedPhoto - 1].alt,
+//         }),
+//       });
+
+//       if (!response.ok) {
+//         throw new Error("Erro ao salvar o voto.");
+//       }
+
+//       // Atualiza localmente os votos
+//       const newVotes = [...votes];
+//       newVotes[selectedPhoto - 1] += 1;
+//       setVotes(newVotes);
+
+//       alert(`Você votou no jogo - ${photos[selectedPhoto - 1].alt}`);
+//     } catch (error) {
+//       console.error(error);
+//       alert("Erro ao registrar o voto. Tente novamente.");
+//     } finally {
+//       setIsVoting(false); // Reativa o botão
+//     }
+//   };
+
+//   const totalVotes = votes.reduce((acc, vote) => acc + vote, 0);
+
+//   return (
+//     <div className={styles.app}>
+//       <header className={styles.header}>
+//         <h1>Qual jogo gostaria de ver em live?</h1>
+//         <p>Você tem 1 voto disponível</p>
+//       </header>
+//       <main className={styles.gallery}>
+//         {photos.map((photo, index) => {
+//           const votePercentage =
+//             totalVotes > 0 ? (votes[index] / totalVotes) * 100 : 0;
+//           return (
+//             <div
+//               key={photo.id}
+//               className={`${styles.photo} ${
+//                 selectedPhoto === photo.id ? styles.selected : ""
+//               }`}
+//               onClick={() => setSelectedPhoto(photo.id)}
+//             >
+//               <Image
+//                 src={photo.src}
+//                 alt={photo.alt}
+//                 width={150}
+//                 height={150}
+//                 className="photo"
+//               />
+//               <div className={styles.photoTextContainer}>
+//                 <p>{photo.alt}</p>
+//               </div>
+//               <p className={styles.percentual}>
+//                 {Math.round(votePercentage)}% - {votes[index]} votos
+//               </p>
+//               <div className={styles.voteBarContainer}>
+//                 <div
+//                   className={styles.voteBar}
+//                   style={{ width: `${votePercentage}%` }}
+//                 ></div>
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </main>
+//       <footer className={styles.footer}>
+//         <button
+//           onClick={handleVote}
+//           className={styles.voteButton}
+//           disabled={isVoting}
+//         >
+//           {isVoting ? "Enviando..." : "Vote"}
+//         </button>
+//       </footer>
+//     </div>
+//   );
+// };
+
+// export default Page;
+
 "use client";
 
 import React, { useState } from "react";
@@ -6,8 +132,9 @@ import Image from "next/image";
 
 const Page: React.FC = () => {
   const [selectedPhoto, setSelectedPhoto] = useState<number | null>(null);
-  const [isVoting, setIsVoting] = useState(false); // Para desativar o botão enquanto vota
+  const [isVoting, setIsVoting] = useState(false);
   const [votes, setVotes] = useState<number[]>(new Array(10).fill(0));
+  const [hasVoted, setHasVoted] = useState(false); // Controle do estado de voto
 
   const photos = [
     { id: 1, src: "/assets/cold-war.jpeg", alt: "Call Of Duty Cold War" },
@@ -36,7 +163,7 @@ const Page: React.FC = () => {
       return;
     }
 
-    setIsVoting(true); // Desativa o botão enquanto processa
+    setIsVoting(true);
 
     try {
       const response = await fetch("/api/vote", {
@@ -48,21 +175,26 @@ const Page: React.FC = () => {
         }),
       });
 
+      const result = await response.json();
+
       if (!response.ok) {
-        throw new Error("Erro ao salvar o voto.");
+        throw new Error(result.message || "Erro ao registrar o voto.");
       }
 
-      // Atualiza localmente os votos
-      const newVotes = [...votes];
-      newVotes[selectedPhoto - 1] += 1;
-      setVotes(newVotes);
-
-      alert(`Você votou no jogo - ${photos[selectedPhoto - 1].alt}`);
+      if (result.message.includes("já votou")) {
+        alert(result.message);
+      } else {
+        setHasVoted(true);
+        const newVotes = [...votes];
+        newVotes[selectedPhoto - 1] += 1;
+        setVotes(newVotes);
+        alert(`Você votou no jogo - ${photos[selectedPhoto - 1].alt}`);
+      }
     } catch (error) {
       console.error(error);
       alert("Erro ao registrar o voto. Tente novamente.");
     } finally {
-      setIsVoting(false); // Reativa o botão
+      setIsVoting(false);
     }
   };
 
@@ -72,7 +204,11 @@ const Page: React.FC = () => {
     <div className={styles.app}>
       <header className={styles.header}>
         <h1>Qual jogo gostaria de ver em live?</h1>
-        <p>Você tem 1 voto disponível</p>
+        <p>
+          {hasVoted
+            ? "Você já votou! Não é mais possível votar novamente."
+            : "Você tem 1 voto disponível"}
+        </p>
       </header>
       <main className={styles.gallery}>
         {photos.map((photo, index) => {
@@ -86,25 +222,15 @@ const Page: React.FC = () => {
               }`}
               onClick={() => setSelectedPhoto(photo.id)}
             >
-              <Image
-                src={photo.src}
-                alt={photo.alt}
-                width={150}
-                height={150}
-                className="photo"
-              />
-              <div className={styles.photoTextContainer}>
-                <p>{photo.alt}</p>
-              </div>
-              <p className={styles.percentual}>
+              <Image src={photo.src} alt={photo.alt} width={150} height={150} />
+              <p>{photo.alt}</p>
+              <p>
                 {Math.round(votePercentage)}% - {votes[index]} votos
               </p>
-              <div className={styles.voteBarContainer}>
-                <div
-                  className={styles.voteBar}
-                  style={{ width: `${votePercentage}%` }}
-                ></div>
-              </div>
+              <div
+                className={styles.voteBar}
+                style={{ width: `${votePercentage}%` }}
+              ></div>
             </div>
           );
         })}
@@ -113,7 +239,7 @@ const Page: React.FC = () => {
         <button
           onClick={handleVote}
           className={styles.voteButton}
-          disabled={isVoting}
+          disabled={isVoting || hasVoted}
         >
           {isVoting ? "Enviando..." : "Vote"}
         </button>
--- src/pages/api/vote.ts
+++ src/pages/api/vote.ts
@@ -1,4 +1,5 @@
 // import dotenv from "dotenv";
+// import { IncomingMessage } from "http";
 // import { NextApiRequest, NextApiResponse } from "next";
 // import { Pool } from "pg";
 // dotenv.config();
@@ -11,6 +12,15 @@
 //       : false,
 // });
 
+// // Função para pegar o IP do usuário, tratando diferentes cabeçalhos
+// const getUserIp = (req: IncomingMessage): string => {
+//   const xForwardedFor = req.headers["x-forwarded-for"];
+//   if (typeof xForwardedFor === "string") {
+//     return xForwardedFor.split(",")[0].trim(); // Garante que o IP seja tratado corretamente
+//   }
+//   return req.socket.remoteAddress || "IP_DESCONHECIDO"; // Fallback para IP desconhecido
+// };
+
 // export default async function handler(
 //   req: NextApiRequest,
 //   res: NextApiResponse
@@ -22,21 +32,33 @@
 //       return res.status(400).json({ message: "Parâmetros inválidos." });
 //     }
 
+//     const userIp = getUserIp(req); // Captura o IP do usuário
+
 //     try {
+//       // Verifica se o IP já votou
+//       const result = await pool.query(
+//         `SELECT * FROM public.votes WHERE user_ip = $1`,
+//         [userIp]
+//       );
+
+//       if (result.rows.length > 0) {
+//         return res.status(400).json({ message: "Você já votou!" });
+//       }
+
+//       // Registra o voto junto com o IP do usuário
 //       await pool.query(
-//         `INSERT INTO public.votes (photo_id, photo_alt, votes, created_at) VALUES ($1, $2, $3, NOW())`,
-//         [photoId, photoAlt, 1]
+//         `INSERT INTO public.votes (photo_id, photo_alt, votes, user_ip, created_at) VALUES ($1, $2, $3, $4, NOW())`,
+//         [photoId, photoAlt, 1, userIp]
 //       );
 //       res.status(200).json({ message: "Voto registrado com sucesso!" });
 //     } catch (error) {
-//       console.error("Erro ao registrar voto:", error);
+//       console.error("Erro ao registrar voto: ", error);
 //       res.status(500).json({ message: "Erro ao registrar o voto." });
 //     }
 //   } else {
 //     res.setHeader("Allow", ["POST"]);
 //     res.status(405).json({ message: `Método ${req.method} não permitido.` });
 //   }
-//
 // }
 
 import dotenv from "dotenv";
@@ -46,20 +68,19 @@ import { Pool } from "pg";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // A URL do banco de dados deve estar no arquivo .env
+  connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
       : false,
 });
 
-// Função para pegar o IP do usuário, tratando diferentes cabeçalhos
 const getUserIp = (req: IncomingMessage): string => {
   const xForwardedFor = req.headers["x-forwarded-for"];
   if (typeof xForwardedFor === "string") {
-    return xForwardedFor.split(",")[0].trim(); // Garante que o IP seja tratado corretamente
+    return xForwardedFor.split(",")[0].trim();
   }
-  return req.socket.remoteAddress || "IP_DESCONHECIDO"; // Fallback para IP desconhecido
+  return req.socket.remoteAddress || "IP_DESCONHECIDO";
 };
 
 export default async function handler(
@@ -73,20 +94,20 @@ export default async function handler(
       return res.status(400).json({ message: "Parâmetros inválidos." });
     }
 
-    const userIp = getUserIp(req); // Captura o IP do usuário
+    const userIp = getUserIp(req);
 
     try {
-      // Verifica se o IP já votou
       const result = await pool.query(
         `SELECT * FROM public.votes WHERE user_ip = $1`,
         [userIp]
       );
 
       if (result.rows.length > 0) {
-        return res.status(400).json({ message: "Você já votou!" });
+        return res.status(200).json({
+          message: "Você já votou! Seu voto foi computado anteriormente.",
+        });
       }
 
-      // Registra o voto junto com o IP do usuário
       await pool.query(
         `INSERT INTO public.votes (photo_id, photo_alt, votes, user_ip, created_at) VALUES ($1, $2, $3, $4, NOW())`,
         [photoId, photoAlt, 1, userIp]
